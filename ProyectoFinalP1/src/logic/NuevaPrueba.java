abe46b5005b9c9a5a3bbbf0248aeccd600b30c0b
joko49perez <joko49perez@x220t>
2019-03-05 15:16:27 -0400

    Prueba de push

---
+++ ProyectoFinalP1/src/logic/NuevaPrueba.java
@@ -0,0 +1,14 @@
+package logic;
+
+public class NuevaPrueba {
+
+	public NuevaPrueba() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Test");
+
+	}
+
+}
